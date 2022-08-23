c727333df870c82c6b540bf23b58f24afa4ef5fb
Max Shipitsyn <m.o.shipitsyn@gmail.com>
2022-08-23 23:14:22 +0600

    grafana nodeport svc working

--- terraform/nlb.tf
+++ terraform/nlb.tf
@@ -12,54 +12,54 @@ resource "yandex_lb_target_group" "nlb-group-grafana" {
   }
 }
 
-resource "yandex_lb_network_load_balancer" "nlb-grafana-db" {
+#resource "yandex_lb_network_load_balancer" "nlb-graf" {
+#
+#  name = "nlb-graf"
+#
+#  listener {
+#    name        = "grafana-listener"
+#    port        = 80
+#    target_port = 30902
+#    external_address_spec {
+#      ip_version = "ipv4"
+#    }
+#  }
+#
+#  attached_target_group {
+#    target_group_id = yandex_lb_target_group.nlb-group-grafana.id
+#
+#    healthcheck {
+#      name = "healthcheck"
+#      tcp_options {
+#        port = 30902
+#      }
+#    }
+#  }
+#  depends_on = [yandex_lb_target_group.nlb-group-grafana]
+#}
 
-  name = "nlb-grafana-db"
-
-  listener {
-    name        = "grafana-listener"
-    port        = 80
-    target_port = 30900
-    external_address_spec {
-      ip_version = "ipv4"
-    }
-  }
-
-  attached_target_group {
-    target_group_id = yandex_lb_target_group.nlb-group-grafana.id
-
-    healthcheck {
-      name = "healthcheck"
-      tcp_options {
-        port = 30900
-      }
-    }
-  }
-  depends_on = [yandex_lb_target_group.nlb-group-grafana]
-}
-
-resource "yandex_lb_network_load_balancer" "nlb-nginx" {
-
-  name = "nlb-nginx"
-
-  listener {
-    name        = "app-listener"
-    port        = 80
-    target_port = 30800
-    external_address_spec {
-      ip_version = "ipv4"
-    }
-  }
-
-  attached_target_group {
-    target_group_id = yandex_lb_target_group.nlb-group-grafana.id
-
-    healthcheck {
-      name = "healthcheck"
-      tcp_options {
-        port = 30800
-      }
-    }
-  }
-  depends_on = [yandex_lb_target_group.nlb-group-grafana]
-}
+#resource "yandex_lb_network_load_balancer" "nlb-nginx" {
+#
+#  name = "nlb-nginx"
+#
+#  listener {
+#    name        = "app-listener"
+#    port        = 80
+#    target_port = 30080
+#    external_address_spec {
+#      ip_version = "ipv4"
+#    }
+#  }
+#
+#  attached_target_group {
+#    target_group_id = yandex_lb_target_group.nlb-group-grafana.id
+#
+#    healthcheck {
+#      name = "healthcheck"
+#      tcp_options {
+#        port = 30080
+#      }
+#    }
+#  }
+#  depends_on = [yandex_lb_target_group.nlb-group-grafana]
+#}
